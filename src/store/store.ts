bfc8d6f92925d46eb742d5426bd92791be54616c
impresence <nikitbreak@gmail.com>
2023-05-27 13:27:54 +0300

    commit

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,44 +5,27 @@ interface ITodo {
     title: string,
     completed: boolean,
 }
-// newTodo сделать объектом
+
 class Store {
-    todos: ITodo[] = [
-        {
-        id: 1,
-        title: 'do homework' ,
-        completed: false,
-        },
-        {
-            id: 2,
-            title: 'do dance' ,
-            completed: false,
-        },
-        {
-            id: 3,
-            title: 'do shit' ,
-            completed: false,
-        }
-    ]
-    newTodo: ITodo = {
-        id: Math.round(Math.random()*1000),
-        title: '',
-        completed: false,
-    }
-    setNewTodoTitle(e: React.ChangeEvent<HTMLInputElement>): void {                     // написать тип
-        this.newTodo.title = e.target.value
-        console.log(this.newTodo.title)
+    todos: ITodo[] = []
+    newTodoTitle: string = '';
+
+    setNewTodoTitle(e: React.ChangeEvent<HTMLInputElement>): void {
+        this.newTodoTitle = e.target.value
+        console.log(this.newTodoTitle)
     }
 
-    addTodo():void {
-        this.todos.push(this.newTodo)
-        console.log(this.todos)
+    addTodo() {
+        const newTodo: ITodo = {
+            id: Math.round(Math.random() * 1000),
+            title: this.newTodoTitle,
+            completed: false,
+        }
+        this.todos.push(newTodo)
+        this.newTodoTitle = ''
+        console.log({...this.todos})
+        
     }
-    
-    // removeTodo() {
-    //     this.todos.pop()
-    //     console.log(this.todos)
-    // }
 
     constructor() {
         makeAutoObservable(this)
